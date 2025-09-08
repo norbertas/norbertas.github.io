5bab7e3fd07aeea89658b604bc1fb856ba145954
norbertas <688266+norbertas@users.noreply.github.com>
2025-09-08 21:03:12 +0300

    Update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'norbertas.com';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'A record of experiments, side projects, and half-finished ideas.';
